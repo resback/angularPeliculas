4fd8474b6ef66a567ff29b2a2ad263937356ec15
resback <resback@gmail.com>
2022-05-26 22:56:58 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'Soy cheems';
+  fecha = new Date();
+
+  peliculas = [
+    {
+      nombre: 'Iron Man',
+      ranking: '10/10',
+    },
+    {
+      nombre: 'Iron Man 2',
+      ranking: '8/10',
+    },
+  ];
 }
